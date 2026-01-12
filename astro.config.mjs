fbc244c8cbb0b404417790cfe80bd6bcc22b776f
Emilio <emilio.angu.2003@gmail.com>
2026-01-12 15:17:32 +0100

    refactor: actualizar configuración de Astro, mejorar el comportamiento del encabezado y ajustar estilos globales

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
+  server: {
+    host: true,
+    port: 4321,
+  },
   adapter: netlify(),
-  integrations: [icon()]
+  integrations: [icon()],
 });
